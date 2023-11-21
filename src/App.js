b7c6321985f035aebba1156d47db71ea1e4b190e
Don Anh Nguyen <thedonwind69@gmail.com>
2023-11-20 20:41:12 -0600

    react dot exercise

--- src/App.js
+++ src/App.js
@@ -1,11 +1,52 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+
+  const [allDots, setAllDots] = useState([]);
+  const [undos, setUndos] = useState([]);
+
+  function handleClickDot (e) {
+    setAllDots((prevState) => [...prevState, {x: e.clientX, y: e.clientY}])
+  }
+
+  function undo () {
+    const lastOne = allDots[allDots.length - 1];
+    setUndos((prevState) => [...prevState, lastOne]);
+    setAllDots((prevState) => {
+      const newArray = prevState.slice(0, -1);
+      return newArray;
+    });
+  }
+
+  function redo () {
+    const lastOne = undos[undos.length - 1];
+    setAllDots((prevState) => [...prevState, lastOne])
+    setUndos((prevState) => {
+      const newArray = prevState.slice(0, -1);
+      return newArray;
+    });
+  }
+
+  console.log('all dots')
+  console.log(allDots)
+  console.log('undos')
+  console.log(undos);
+
   return (
-    <div className="App">
+  <div>
+    <button disabled={!allDots.length} onClick={undo}>Undo</button><button disabled={!undos.length} onClick={redo}>Redo</button>
+    <div onClick={(e) => handleClickDot(e)} className="App" style={{position: 'relative'}}>
+
     
+
+        {allDots.map((dot, i) => {
+          return <span key={i} style={{color: 'blue', position: 'absolute', fontSize: '2em', left: `${dot.x}px`, top: `${dot.y}px`}}>o</span>
+        })}
+
     </div>
+  </div>
+  
   );
 }
 
